419ac5571e41e731a05a470bf2eeec8130e927eb
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-05-28 16:50:56 +0100

    added flat model

---
+++ db/migrate/20210528153758_create_flats.rb
@@ -0,0 +1,11 @@
+class CreateFlats < ActiveRecord::Migration[6.1]
+  def change
+    create_table :flats do |t|
+      t.string :address
+      t.integer :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
