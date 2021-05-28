1e34bfdcf612a1382d407b2333495ee3c2a5b8ef
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-05-28 17:12:29 +0100

    user can create a new flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,11 +4,25 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :price, :user_id)
   end
 end
