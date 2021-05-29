b95e8ec93ac2d98bfd766a3b9714f34e859ef48d
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-05-29 15:08:32 +0100

    cleaned up navbar active class logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Flat.destroy_all
+User.destroy_all
+
+require "open-uri"
+
 user = User.create!(email:'test@test.com', password: 'secret')
 
-Flat.create!(name: 'Terrace House', address: 'Tokyo', description: 'Sleeps six in the heart of Tokyo', price: '1000', user_id: user.id)
+tokyo = Flat.create!(name: 'Terrace House', address: 'Tokyo', description: 'Sleeps six in the heart of Tokyo', price: '1000', user_id: user.id)
+
+tokyo_array = ['http://www.terrace-house.jp/inthecity/photo/gallery1_1.jpg','http://www.terrace-house.jp/inthecity/photo/gallery1_2.jpg', 'http://www.terrace-house.jp/inthecity/photo/gallery1_3.jpg', 'http://www.terrace-house.jp/inthecity/photo/gallery1_4.jpg', 'http://www.terrace-house.jp/inthecity/photo/gallery1_5.jpg']
+
+tokyo_array.each do |url|
+  file = URI.open(url)
+  tokyo.photos.attach(io: file, filename: 'flat.png', content_type: 'image/png')
+end
