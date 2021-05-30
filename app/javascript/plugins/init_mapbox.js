168db93b406246411ed8a7a8c6487fd489a004ce
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-05-30 16:59:31 +0100

    mapbox display fixes

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -18,15 +18,12 @@ const initMapbox = () => {
   });
   fitMapToMarkers(map, markers);
   }
-
-
-
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
-};
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  };
 
 export { initMapbox };
