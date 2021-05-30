ef6e12ad4cc1e7ff734c3ab562fa5156a8fcf179
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-05-30 17:03:04 +0100

    fixed navlinks to make turbolinks happier

--- app/javascript/packs/init_nav_links.js
+++ app/javascript/packs/init_nav_links.js
@@ -1,26 +1,28 @@
 const initNavLinks = () => {
-
-  const flats = document.querySelector('.flats')
-  const bookings = document.querySelector('.bookings')
   const tabs = document.querySelectorAll('.list-inline tabs-underlined')
+    const flats = document.querySelector('.flats')
+    const bookings = document.querySelector('.bookings')
 
-  const flatTab = document.querySelector('#tab-flat')
-  const bookingTab = document.querySelector('#tab-booking')
+    const flatTab = document.querySelector('#tab-flat')
+    const bookingTab = document.querySelector('#tab-booking')
 
- flatTab.addEventListener('click', tab => {
-    event.preventDefault();
-    bookingTab.classList.remove('active')
-    bookings.classList.remove('active')
-    event.currentTarget.classList.add('active')
-    flats.classList.add('active');
- })
+ if (flatTab) {
+   flatTab.addEventListener('click', tab => {
+      event.preventDefault();
+      bookingTab.classList.remove('active')
+      bookings.classList.remove('active')
+      event.currentTarget.classList.add('active')
+      flats.classList.add('active');
+   })
 
- bookingTab.addEventListener('click', tab => {
-    event.preventDefault();
-    event.currentTarget.classList.add('active')
-    flatTab.classList.remove('active')
-    flats.classList.remove('active')
-    bookings.classList.add('active');
- })
+   bookingTab.addEventListener('click', tab => {
+      event.preventDefault();
+      event.currentTarget.classList.add('active')
+      flatTab.classList.remove('active')
+      flats.classList.remove('active')
+      bookings.classList.add('active');
+   })
+ }
 }
+
 export { initNavLinks };
