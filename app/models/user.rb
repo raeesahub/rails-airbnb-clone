11214c73a4d5342181671e4853a55ba641164b61
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-06-02 00:00:00 +0100

    update user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :flats
   has_many :bookings
+  has_many :reviews, through: :orders
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
